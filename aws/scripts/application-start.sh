1c5653ee938614c60b5e6cb59685d1afcbc28a84
Cristhiano Roberto <bangomc@gmail.com>
2023-08-09 21:09:12 -0300

    Copiando jar e executando

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -3,4 +3,4 @@ set -xe
 
 # Start Tomcat, the application server.
 #service tomcat start
-java -jar /usr/local/app/listacomprathymeleaf-0.0.1-SNAPSHOT.jar
+nohup java -jar /usr/local/app/listacomprathymeleaf-0.0.1-SNAPSHOT.jar &
